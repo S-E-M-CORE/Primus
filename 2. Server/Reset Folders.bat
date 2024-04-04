b728ec4489a989146464d219eca4478734fb3d59
Sascha Meissner <dersaschameissner@web.de>
2024-04-04 16:07:03 +0200

    Umlaute entfernt
    ö zu oe

--- 2. Server/Reset Folders.bat
+++ 2. Server/Reset Folders.bat
@@ -3,27 +3,27 @@ cd /d %~dp0
 
 set "deleted=false"
 
-echo Lösche Ordner "bin"...
+echo Loesche Ordner "bin"...
 if exist "bin\*" (
     rd /s /q "bin"
     set "deleted=true"
 )
 
-echo Lösche Ordner "out"...
+echo Loesche Ordner "out"...
 if exist "out\*" (
     rd /s /q "out"
     set "deleted=true"
 )
 
-echo Lösche Ordner ".vs"...
+echo Loesche Ordner ".vs"...
 if exist ".vs\*" (
     rd /s /q ".vs"
     set "deleted=true"
 )
 
 if %deleted%==true (
-    echo Die angegebenen Ordner wurden gelöscht.
+    echo Die angegebenen Ordner wurden geloescht.
 ) else (
-    echo Es wurden keine Ordner gelöscht, da sie entweder nicht existieren oder Dateien enthalten.
+    echo Es wurden keine Ordner geloescht.da sie entweder nicht existieren oder Dateien enthalten.
 )
 pause
