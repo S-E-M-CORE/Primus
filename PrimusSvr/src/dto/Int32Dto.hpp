4238bc9add40e3119174a8af0e43b02527e96ba9
Sascha Meissner <dersaschameissner@web.de>
2024-04-06 08:05:39 +0200

    Neue Dtos zum Darstellen eines einzelnen Werts (signed und unsigned)

---
+++ PrimusSvr/src/dto/Int32Dto.hpp
@@ -0,0 +1,49 @@
+#ifndef COUNTDTO_HPP
+#define COUNTDTO_HPP
+
+#include "oatpp/core/Types.hpp"
+#include "oatpp/core/macro/codegen.hpp"
+
+namespace primus
+{
+    namespace dto
+    {
+
+#include OATPP_CODEGEN_BEGIN(DTO)
+
+        /**
+         * @brief DTO class representing single value.
+         */
+        class UInt32Dto : public oatpp::DTO
+        {
+
+            DTO_INIT(UInt32Dto, DTO);
+
+            DTO_FIELD_INFO(value) {
+                info->description = "General value of a single unsigned int";
+            }
+            DTO_FIELD(oatpp::UInt32, value);
+
+        };
+
+        /**
+         * @brief DTO class representing single value.
+         */
+        class Int32Dto : public oatpp::DTO
+        {
+
+            DTO_INIT(Int32Dto, DTO);
+
+            DTO_FIELD_INFO(value) {
+                info->description = "General value of a single unsigned int";
+            }
+            DTO_FIELD(oatpp::Int32, value);
+
+        };
+
+#include OATPP_CODEGEN_END(DTO)
+
+    } // namespace dto
+} // namespace primus
+
+#endif // COUNTDTO_HPP
