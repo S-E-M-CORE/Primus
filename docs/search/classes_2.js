b518e1b91f6d3467d478278b0f0ceb89a65159f9
SEM <72030464+theSaschaM@users.noreply.github.com>
2024-04-11 16:14:37 +0200

    Add files via upload

---
+++ docs/search/classes_2.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['databaseclient_0',['DatabaseClient',['../classprimus_1_1component_1_1_database_client.html',1,'primus::component']]],
+  ['databasecomponent_1',['DatabaseComponent',['../classprimus_1_1component_1_1_database_component.html',1,'primus::component']]],
+  ['datedto_2',['DateDto',['../classprimus_1_1dto_1_1database_1_1_date_dto.html',1,'primus::dto::database']]],
+  ['departmentdto_3',['DepartmentDto',['../classprimus_1_1dto_1_1database_1_1_department_dto.html',1,'primus::dto::database']]]
+];
---
+++ docs/search/classes_5.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['pagedto_0',['PageDto',['../classprimus_1_1dto_1_1_page_dto.html',1,'primus::dto']]],
+  ['pagedto_3c_20oatpp_3a_3aobject_3c_20primus_3a_3adto_3a_3adatabase_3a_3amemberdto_20_3e_20_3e_1',['PageDto&lt; oatpp::Object&lt; primus::dto::database::MemberDto &gt; &gt;',['../classprimus_1_1dto_1_1_page_dto.html',1,'primus::dto']]]
+];
