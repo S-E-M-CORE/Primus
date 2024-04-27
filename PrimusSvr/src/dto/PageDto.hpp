9e1639bae037f5c05bed016d5236d3baed6a0d30
Sascha Meissner <dersaschameissner@web.de>
2024-04-27 12:59:59 +0200

    Replaced MemberPageDto
    Replaced the class MemberPageDto and added type aliases for typically used pages (like members, addresses, departments and dates)

--- PrimusSvr/src/dto/PageDto.hpp
+++ PrimusSvr/src/dto/PageDto.hpp
@@ -21,38 +21,36 @@ namespace primus
 
             DTO_INIT(PageDto, DTO);
 
+            DTO_FIELD(UInt32, offset);
             DTO_FIELD_INFO(offset) {
                 info->description = "Offset value for pagination";
+                info->required = true;
             }
-            DTO_FIELD(UInt32, offset);
 
+            DTO_FIELD(UInt32, limit);
             DTO_FIELD_INFO(limit) {
                 info->description = "Limit value for pagination";
+                info->required = true;
             }
-            DTO_FIELD(UInt32, limit);
 
+            DTO_FIELD(UInt32, count);
             DTO_FIELD_INFO(count) {
                 info->description = "Total count of items";
+                info->required = true;
             }
-            DTO_FIELD(UInt32, count);
 
+            DTO_FIELD(Vector<T>, items);
             DTO_FIELD_INFO(items) {
                 info->description = "List of items";
             }
-            DTO_FIELD(Vector<T>, items);
 
         };
-        //  __  __                _               ____                  ____  _        
-        // |  \/  | ___ _ __ ___ | |__   ___ _ __|  _ \ __ _  __ _  ___|  _ \| |_ ___  
-        // | |\/| |/ _ \ '_ ` _ \| '_ \ / _ \ '__| |_) / _` |/ _` |/ _ \ | | | __/ _ \ 
-        // | |  | |  __/ | | | | | |_) |  __/ |  |  __/ (_| | (_| |  __/ |_| | || (_) |
-        // |_|  |_|\___|_| |_| |_|_.__/ \___|_|  |_|   \__,_|\__, |\___|____/ \__\___/ 
-        //                                                   |___/                     
-        class MemberPageDto : public PageDto<oatpp::Object<primus::dto::database::MemberDto>> {
 
-            DTO_INIT(MemberPageDto, PageDto<oatpp::Object<primus::dto::database::MemberDto>>)
+        using MemberPageDto     = primus::dto::PageDto<oatpp::Object<primus::dto::database::MemberDto>>;
+        using AddressPageDto    = primus::dto::PageDto<oatpp::Object<primus::dto::database::AddressDto>>;
+        using DepartmentPageDto = primus::dto::PageDto<oatpp::Object<primus::dto::database::DepartmentDto>>;
+        using DatePageDto       = primus::dto::PageDto<oatpp::Object<primus::dto::database::DateDto>>;
 
-        };
 #include OATPP_CODEGEN_END(DTO)
 
     } // namespace dto
